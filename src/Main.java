13effcfa6bf0e8b63643f8b2e541f96410442dca
BrycenP <perezb3@mail.uc.edu>
2024-12-08 18:45:18 -0500

    working and tested part 1

--- src/Main.java
+++ src/Main.java
@@ -11,42 +11,42 @@ public class Main
         JFileChooser chooser = new JFileChooser("src");
         File selectedFile;
         String rec = "";
-        chooser.setCurrentDirectory(new File(System.getProperty("user.dir")+"/src"));
-
-        try {
-            File workingDirectory = new File(System.getProperty("user.dir"));
-
-            chooser.setCurrentDirectory(workingDirectory);
-
+        if (args.length > 0) {
+            selectedFile = new File(args[0]);
+            if (!selectedFile.exists()) {
+                System.out.println("The file specified does not exist.");
+                return;
+            }
+        } else {
+            chooser.setCurrentDirectory(new File(System.getProperty("user.dir") + "/src"));
             if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
                 selectedFile = chooser.getSelectedFile();
-                Path file = selectedFile.toPath();
-
-                InputStream in = new BufferedInputStream(Files.newInputStream(file));
-                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
-                int lines = 0;
-                int wordsNum = 0;
-                int characters = 0;
-                while (reader.ready()) {
-                    rec = reader.readLine();
-                    lines++;
-                    String [] words = rec.split(",\\s");
-                    wordsNum += words.length;
-                    characters += rec.length();
-                    System.out.printf("\nLine %4d %-60s ", lines, rec);
-                }
-                reader.close();
-                System.out.println("\n\nData file read!");
-                System.out.println("Name: " + selectedFile.getName());
-                System.out.println("Word count: " +wordsNum);
-                System.out.println("Line count: " +lines);
-                System.out.println("Character count: " + characters);
-
-            } else
-            {
+            } else {
                 System.out.println("No file selected!!! ... exiting.\nRun the program again and select a file.");
+                return;
             }
+        }
+        try {
+            Path file = selectedFile.toPath();
+            InputStream in = new BufferedInputStream(Files.newInputStream(file));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            int lines = 0;
+            int wordsNum = 0;
+            int characters = 0;
+            while (reader.ready()) {
+                rec = reader.readLine();
+                lines++;
+                String [] words = rec.split(",\\s");
+                wordsNum += words.length;
+                characters += rec.length();
+                System.out.printf("\nLine %4d %-60s ", lines, rec);
+            }
+            reader.close();
+            System.out.println("\n\nData file read!");
+            System.out.println("Name: " + selectedFile.getName());
+            System.out.println("Word count: " +wordsNum);
+            System.out.println("Line count: " +lines);
+            System.out.println("Character count: " + characters);
         }
         catch (FileNotFoundException e)
         {
